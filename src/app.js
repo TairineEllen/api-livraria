9589c9220747ba68f43718482279d3e97d942a0b
TairineEllen <tairine.ellen@gmail.com>
2020-11-04 21:38:59 -0300

    created server and app

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.use('/', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Alow-Headers',
+    'Origin', 'X-Request-With', 'Content-Type', 'Accept'
+  );
+  console.info('Nova requisição realizada');
+  next();
+});
+
+module.exports = app;
