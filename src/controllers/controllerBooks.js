b1502dd702c89f5f72a65e168de4e13c938d0cf7
TairineEllen <tairine.ellen@gmail.com>
2020-11-05 09:35:16 -0300

    created route and function to delete a book (DELETE/livros/:id)

--- src/controllers/controllerBooks.js
+++ src/controllers/controllerBooks.js
@@ -1,13 +1,13 @@
 const books = require('../models/books.json');
-const fs = require('fs')
+const fs = require('fs');
 
-const createJsonFile = mes => {
+const updateJsonFile = mess => {
   fs.writeFile('./src/models/books.json', JSON.stringify(books), 'utf-8', err => {
-    err ? res.status(424).send({ message: err.message }) : console.info(mes)
+    err ? res.status(424).send({ message: err.message }) : console.info(mess)
   });
 };
 
-const postBook = (req, res) => {
+const registerBook = (req, res) => {
   let id = 1;
 
   if (books.length > 0) {
@@ -15,12 +15,35 @@ const postBook = (req, res) => {
   };
   const { titulo, autoria, editora, emEstoque } = req.body;
 
-  books.push({ id, titulo, autoria, editora, emEstoque });  
+  books.push({ id, titulo, autoria, editora, emEstoque });
 
-  createJsonFile('Livro registrado com sucesso');  
+  updateJsonFile('Livro registrado com sucesso');
   res.status(201).send(books);
 };
 
+const deleteBook = (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const bookToBeDeleted = books.find(book => book.id == id);
+    const index = books.indexOf(bookToBeDeleted);
+
+    if (index >= 0) {
+      books.splice(index, 1);
+
+      updateJsonFile('Livro deletado com sucesso');
+      res.status(200).send('Livro deletado com sucesso');
+
+    } else {
+      res.status(404).send('Livro não encontrado');
+    };
+
+  } catch (error) {
+    res.status(424).send('Erro interno no servidor');
+  };
+};
+
 module.exports = {
-  postBook
+  registerBook,
+  deleteBook
 };
--- src/routes/routeBooks.js
+++ src/routes/routeBooks.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/controllerBooks');
 
-router.post('/', controller.postBook);
+router.post('/', controller.registerBook);
+router.delete('/:id', controller.deleteBook);
 
 module.exports = router;
